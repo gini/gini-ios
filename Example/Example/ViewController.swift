2cacd8113ba02054d5088fcb98e8696338d8e941
Kike <enrique@gini.net>
2019-03-04 12:38:55 +0100

    Added Example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Enrique del Pozo Gómez on 3/4/19.
+//  Copyright © 2019 Gini GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
