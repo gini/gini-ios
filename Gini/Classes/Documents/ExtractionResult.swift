f2ee9905ea32d171acb9b89cb86b236ce87b624a
maciejtrybilo <maciej@gini.net>
2020-02-26 13:54:29 +0100

    Documentation for ExtractionResult.

--- Gini/Classes/Documents/ExtractionResult.swift
+++ Gini/Classes/Documents/ExtractionResult.swift
@@ -7,9 +7,15 @@
 
 import Foundation
 
+/**
+* Data model for a document extraction result.
+*/
 @objcMembers final public class ExtractionResult: NSObject {
 
+    /// The specific extractions.
     public let extractions: [Extraction]
+    
+    /// The line item compound extractions.
     public var lineItems: [[Extraction]]?
     
     public init(extractions: [Extraction], lineItems: [[Extraction]]?) {
